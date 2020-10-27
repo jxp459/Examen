c6d75d7875b3b76bccfe4d7e9c19fe700072c12e
jxp459 <juni1997parraga@gmail.com>
2020-10-27 00:27:27 -0500

    primer commit

---
+++ JuniorParragaExamen/Resumen.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JuniorParragaExamen
+{
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string nombre, string pago)
+        {
+            InitializeComponent();
+            txtNombre.Text = nombre;
+            txtUsuario.Text = usuario;
+            txtPagoTotal.Text = pago;
+        }
+    }
+}
